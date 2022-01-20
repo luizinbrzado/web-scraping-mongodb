554858566739284533606c87dcf50c29b0281284
luizinbrzado <luiztrineves@gmail.com>
2022-01-20 06:41:41 -0300

    Deixando código menos verboso

--- server.js
+++ server.js
@@ -39,15 +39,7 @@ routes(app);
 
 console.log("Rodando web scraping");
 
-var ultimoCache = -123;
-var doisCache = -123;
-var tresCache = -123;
-var quatroCache = -123;
-
 var numeroUltimo = -987;
-var numeroDois = -987;
-var numeroTres = -987;
-var numeroQuatro = -987;
 
 (async function blazeBot() {
 
@@ -70,7 +62,9 @@ var numeroQuatro = -987;
     await driver.get('https://blaze.com/pt/games/double')
     await driver.manage().window().maximize()
 
-    await driver.sleep(3000)
+    await driver.sleep(5000)
+
+    var i = 0;
 
     while (true) {
         const now = new Date();
@@ -84,67 +78,43 @@ var numeroQuatro = -987;
 
         try {
             var isCompleted = await driver.findElement(webdriver.By.xpath('//*[@id="roulette"]')).getAttribute('class');
-            var classUltimo = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div')).getAttribute('class');
-            var classDois = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[2]/div/div')).getAttribute('class');
-            var classTres = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[3]/div/div')).getAttribute('class');
-            var classQuatro = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[4]/div/div')).getAttribute('class');
 
             if (isCompleted.includes('complete')) {
                 await driver.sleep(2000)
-
+                var classUltimo = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div')).getAttribute('class');
                 classUltimo.includes('white') ? numeroUltimo = '0' : numeroUltimo = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div/div')).getText();
-                classDois.includes('white') ? numeroDois = '0' : numeroDois = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[2]/div/div/div')).getText();
-                classTres.includes('white') ? numeroTres = '0' : numeroTres = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[3]/div/div/div')).getText();
-                classQuatro.includes('white') ? numeroQuatro = '0' : numeroQuatro = await driver.findElement(webdriver.By.xpath('//*[@id="roulette-recent"]/div/div[1]/div[4]/div/div/div')).getText();
+
+                if (numeroUltimo >= 0) {
+                    var resultado = {
+                        "time": now.toLocaleTimeString('pt-br').slice(0, 5),
+                        "result": numeroUltimo
+                    };
+
+                    request({
+                        url: `http://localhost:${port}/resultados`,
+                        method: "POST",
+                        json: true,   // <--Very important!!!
+                        body: resultado
+                    }, function (error, response, body) {
+                    });
+
+                    request({
+                        url: `http://localhost:${port}/hoje`,
+                        method: "POST",
+                        json: true,   // <--Very important!!!
+                        body: resultado
+                    }, function (error, response, body) {
+                    });
+
+
+                    console.log("Adicionando", numeroUltimo, now.toLocaleTimeString('pt-br'));
+                    await driver.sleep(25 * 1000)
+                }
             }
         } catch (e) {
-            console.log(e);
+            console.log("Deu erro");
             process.exit(0)
         }
-
-        if (ultimoCache === numeroUltimo &&
-            doisCache === numeroDois &&
-            tresCache === numeroTres &&
-            quatroCache === numeroQuatro) {
-
-            await new Promise(resolve => setTimeout(resolve, 1000))
-
-        } else {
-
-            if (numeroUltimo >= 0) {
-                var resultado = {
-                    "time": now.toLocaleTimeString('pt-br').slice(0, 5),
-                    "result": numeroUltimo
-                };
-
-                request({
-                    url: `http://localhost:${port}/resultados`,
-                    method: "POST",
-                    json: true,   // <--Very important!!!
-                    body: resultado
-                }, function (error, response, body) {
-                });
-
-                request({
-                    url: `http://localhost:${port}/hoje`,
-                    method: "POST",
-                    json: true,   // <--Very important!!!
-                    body: resultado
-                }, function (error, response, body) {
-                });
-
-
-                console.log("Adicionando", numeroUltimo, now.toLocaleTimeString('pt-br'));
-            }
-
-            ultimoCache = numeroUltimo;
-            doisCache = numeroDois;
-            tresCache = numeroTres;
-            quatroCache = numeroQuatro;
-
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 1000))
 
     }
 })()
